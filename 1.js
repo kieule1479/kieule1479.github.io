b02edef6b0c024c52fa806777b8f337377c18d45
Kiều Lê <kieule1479@gmail.com>
2021-05-20 23:29:33 +0700

    up lan 1

--- 1.js
+++ 1.js
@@ -1,4 +1,15 @@
-﻿ $(function(){
- 
-})  
- 
+﻿$(function () {
+  $(".xuong,.kn").click(function () {
+    $("body,html").animate({ scrollTop: $("#khoiedu").offset().top }, 700);
+    return false;
+  });
+  $(".sp").click(function () {
+    $("body,html").animate({ scrollTop: $("#khoisanpham").offset().top }, 700);
+    return false;
+  });
+
+  $(".cm").click(function () {
+    $("body,html").animate({ scrollTop: $("#phanhoi").offset().top }, 700);
+    return false;
+  });
+});
